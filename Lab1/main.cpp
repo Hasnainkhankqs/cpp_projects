d5dc489c206296318a78f5253537e847c42ed15c
Hasnain Khan <hasnainkhankqs@gmail.com>
2018-04-22 19:09:07 +0500

    first program almost complete

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -4,5 +4,12 @@ using namespace std;
 
 int main()
 {
-
+cout << "\xC9";
+for(int a = 0 ; a<= 40; a++)
+cout << "\xCD";
+cout <<"\xBB";
+cout << "\n\xBA Learning C programming";
+cout << "\n\xC8";
+for(int a = 0 ; a<= 40; a++)
+cout << "\xCD";
 }
