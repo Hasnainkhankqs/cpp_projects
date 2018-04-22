1879c838f1c08d96b371a6391893b8829fef094f
Hasnain Khan <hasnainkhankqs@gmail.com>
2018-04-23 00:33:52 +0500

    properly work started

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -1,9 +1,35 @@
 #include <iostream>
 
 using namespace std;
-
+void whichprogram();
+void diagram();
+void factorials();
 int main()
 {
+    whichprogram();
+    return 0;
+}
+
+void whichprogram(){
+    int bring;
+    cout << "Enter a number to find program" << endl;
+    cout << "1 - Diagram Learning C " << endl;
+    cout << "2 - The factorials of integers given by user" << endl;
+    cin >> bring;
+    if(bring == 1 ){
+        diagram();
+    }
+    if(bring == 2){
+        factorials();
+    }
+
+}
+
+
+
+void diagram(){
+
+//   box characters https://www.wikiwand.com/en/Box-drawing_character
 cout << "\xC9";
 for(int a = 0 ; a<= 35; a++)
 cout << "\xCD";
@@ -18,10 +44,11 @@ for(int a = 0 ; a< 5; a++)
     }
 
 }
-
-
 cout << "\n\xC8";
 for(int a = 0 ; a<= 35; a++)
 cout << "\xCD";
 cout << "\xBC";
+
 }
+
+
