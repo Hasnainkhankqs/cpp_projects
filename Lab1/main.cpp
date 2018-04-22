fce49a7ec610485dd9d09a506f7a25f5195e4ef7
Hasnain Khan <hasnainkhankqs@gmail.com>
2018-04-22 19:35:49 +0500

    task1 completed

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -5,11 +5,23 @@ using namespace std;
 int main()
 {
 cout << "\xC9";
-for(int a = 0 ; a<= 40; a++)
+for(int a = 0 ; a<= 35; a++)
 cout << "\xCD";
 cout <<"\xBB";
-cout << "\n\xBA Learning C programming";
+for(int a = 0 ; a< 5; a++)
+{
+    if(a == 2){
+        cout << "\n\xBA        Learning C programming      \xBA";
+    }
+    else{
+        cout << "\n\xBA                                    \xBA";
+    }
+
+}
+
+
 cout << "\n\xC8";
-for(int a = 0 ; a<= 40; a++)
+for(int a = 0 ; a<= 35; a++)
 cout << "\xCD";
+cout << "\xBC";
 }
