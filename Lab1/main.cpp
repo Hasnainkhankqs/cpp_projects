8fc43c25b27c689bbfa150f5b476771e14c78681
Hasnain Khan <hasnainkhankqs@gmail.com>
2018-05-09 03:50:41 -0700

    seven tasks completed eigth started

--- Lab1/main.cpp
+++ Lab1/main.cpp
@@ -5,25 +5,49 @@ void whichprogram();
 void diagram();
 void factorials();
 void arrysorting();
-void temperature();
-int main()
-{
-int a[3][4] = {
-   {0, 1, 2, 3} ,   /*  initializers for row indexed by 0 */
-   {4, 5, 6, 7} ,   /*  initializers for row indexed by 1 */
-   {8, 9, 10, 11}   /*  initializers for row indexed by 2 */
-};
+void leftPattern();
+void numSquare();
+void desiredFactorial();
+void xpower();
+void numberPattern();
 
-   //whichprogram();
+
+int main(){
+
+for(int a = 10; a > 1; a--){
+    for(int b = a ; b > 1 ; b--){
+        cout << "*";
+    }
+    for(int c = 1 ; c <= a ; c++){
+        cout << "+";
+    }
+    cout << endl;
+}
+for(int a = 1; a <= 10; a++){
+    for(int b = 1 ; b <= a ; b++){
+        cout << "*";
+    }
+    cout << endl;
+}
+  // whichprogram();
     return 0;
 }
-
+//which program do you want to execute
 void whichprogram(){
+
     int bring;
+
     cout << "Enter a number to find program" << endl;
     cout << "1 - Diagram Learning C " << endl;
     cout << "2 - The factorials of integers given by user" << endl;
     cout << "3 - Sorting" << endl;
+    cout << "4 - Left pattern" << endl;
+    cout << "5 - Square of numbers up-to 100" << endl;
+    cout << "6 - Desired Factorial" << endl ;
+    cout << "7 - Y to the power X" << endl ;
+    cout << "8 - Number pattern" << endl ;
+
+
     cin >> bring;
     if(bring == 1 ){
         diagram();
@@ -34,11 +58,31 @@ void whichprogram(){
     if(bring == 3){
         arrysorting();
     }
-     if(bring == 4){
-        temperature();
+    if(bring == 4){
+        leftPattern();
+    }
+    if(bring == 5){
+        numSquare();
+    }
+    if(bring == 6){
+        desiredFactorial();
+    }
+    if(bring == 7){
+        xpower();
+    }
+    if(bring == 8){
+       numberPattern();
     }
 
 }
+
+
+
+
+
+
+
+
 
 
 // diagram program
@@ -63,10 +107,31 @@ cout << "\n\xC8";
 for(int a = 0 ; a<= 35; a++)
 cout << "\xCD";
 cout << "\xBC";
-
+ whichprogram();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //factorials program
 void factorials(){
+//sort array up-to your desired numbers
+//array sorting
 //array sorting program
 
 int c = 1;
@@ -83,8 +148,23 @@ int c = 1;
     }
     cout << c;
     }
-
+ whichprogram();
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 void arrysorting(){
  int i,sortingarr[5],temp,j;
 
@@ -118,12 +198,137 @@ void arrysorting(){
  {
  cout<<sortingarr[j];
  }
-
-}
-
-void temperature(){
-
-
+ whichprogram();
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+//left pattern
+void leftPattern(){
+    int outer , inner,lines;
+    cout << "Enter amount of line to be printed";
+    cin >> lines;
+    for(outer=0 ; outer < lines ; outer++){
+        for(inner=0 ; inner <= outer ; inner++ ){
+            cout << "*";
+        }
+        cout << endl;
+    }
+ whichprogram();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void numSquare(){
+
+int a ,b ;
+b=1;
+for(a = 1; a < 101 ; a++){
+    b = a * a;
+    cout << b << endl;
+}
+ whichprogram();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+void desiredFactorial(){
+    int num , a , b;
+    b = 1;
+    cout << "Enter number for to be factorial";
+    cin >> num;
+    for(a = 1 ; a <= num  ; a++){
+            b = b * a;
+    }
+    cout << b ;
+
+}
+
+
+
+
+
+
+
+
+
+void xpower(){
+
+int x,y,b ;
+b=1;
+cout << "Enter y";
+cin >> y;
+cout << "Enter x";
+cin >> x;
+
+for(int a = 1; a <= x ; a++){
+
+ b =  b * y;
+}
+cout << b;
+}
+
+
+
+
+
+
+
+
+
+
+
+void numberPattern(){
+
+
+}
